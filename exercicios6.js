7c01ca239180818b6d5416cf0a491bf861da10f2
Guilherme Paredes <guilhermeparedes@yahoo.com.br>
2025-03-21 16:00:58 -0300

    codigo finalizado

--- exercicios6.js
+++ exercicios6.js
@@ -1,6 +1,13 @@
 
-    getRandomInt(1, 10); {
-        const minCeiled = Math.ceil(1);
-        const maxFloored = Math.floor(10);
-        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
-    }
+let a = prompt("adivinhe o numero entre 1 e 10:")
+let x = 0
+let numeroSorteado;
+const minCeiled = Math.ceil(1);
+const maxFloored = Math.floor(10);
+numeroSorteado = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+while (a != numeroSorteado) {
+    console.log("ns " + numeroSorteado);
+    a = prompt("Você errou, Tente denovo:")
+    x++
+}
+alert("você acertou! você tentou um total de " + x + "x até acertar o numero " + numeroSorteado + " , Parabéns!")
