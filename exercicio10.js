e548ceb9b2db8cf93bdedc8d3656b2499da2652f
Guilherme Paredes <guilhermeparedes@yahoo.com.br>
2025-03-25 14:36:27 -0300

    att de codigo

--- exercicio10.js
+++ exercicio10.js
@@ -1,12 +1,13 @@
 exercicio10.js
+let np = Number(prompt("informe a quantidade de provas:"))
 let n1 = Number(prompt("Informe a nota:"))
 let provas = 0; 
 let totaldeprovas = 1;
-while(totaldeprovas <= 5){
+while(totaldeprovas <= np){
   n1 = Number(prompt("Informe a Proxima nota:"))
 provas += n1; 
-console.log(provas)
+console.log("provas" + provas)
 totaldeprovas++
-console.log(totaldeprovas)
+console.log("Total de provas" + totaldeprovas)
 } 
 alert("Sua Média é: " + provas / totaldeprovas)
